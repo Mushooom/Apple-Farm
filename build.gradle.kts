1255b070e9a9d96aae153137604ef54512c4c6d1
Alexander <62615349+Mushooom@users.noreply.github.com>
2023-11-25 15:01:46 +0000

    Fist commit Add structure, new packages and files Add dependencies Update Gradle build

---
+++ build.gradle.kts
@@ -0,0 +1,51 @@
+plugins {
+    kotlin("jvm") version "1.9.20"
+    // Plugin for Dokka -> KDoc
+    id("org.jetbrains.dokka") version "1.9.10"
+    jacoco
+    // Plugin for Ktlint
+    id("org.jlleitschuh.gradle.ktlint") version "11.2.0"
+    application
+}
+
+group = "me.alnovovakovsky"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    // Logging
+    implementation ("org.slf4j:slf4j-simple:1.7.36")
+    implementation ("io.github.microutils:kotlin-logging:2.1.23")
+    // SML and JSON
+    implementation("com.thoughtworks.xstream:xstream:1.4.20")  // Xstream from maven
+    implementation("org.codehaus.jettison:jettison:1.5.4")
+
+    // Dokka
+    implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.9.10")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.jar {
+    manifest.attributes["Main-Class"] = "MainKt"
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    from(sourceSets.main.get().output)
+    dependsOn(configurations.runtimeClasspath)
+    from({
+        configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+    })
+}
+
+kotlin {
+    jvmToolchain(8)
+}
+
+application {
+    mainClass.set("MainKt")
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+// Alexander Novakovsky Apple Farm App
+
+
+// Main function
+fun main() {
+    println("Hello World!")
+
+    // Try adding program arguments via Run/Debug configuration.
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+    println("Program arguments:")
+}
